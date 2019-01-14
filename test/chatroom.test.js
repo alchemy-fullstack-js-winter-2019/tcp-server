0f3c0ceb2e0236714a7558e1e4ff4633e8c1e0af
Paige Gorry <paigeegorry@gmail.com>
2019-01-14 15:07:33 -0800

    test is correct on renames a user?

--- test/chatroom.test.js
+++ test/chatroom.test.js
@@ -5,18 +5,27 @@ describe('Chatroom', () => {
   beforeEach(() => {
     chatroom = new Chatroom();
   });
+
   it('can add a client', () => {
     //call the chatroom.add method on a client
     const client = {};
     chatroom.add(client);
     expect(client.username).toBeDefined();
   });
+
   it('returns the client with said username', () => {
     const client = {};
     chatroom.add(client);
-    const username = client.username;
-    const chosen = chatroom.getClient(username);
+    const chosen = chatroom.getClient(client.username);
     expect(chosen.username).toEqual(client.username);
   });
+
+  it('renames a user', () => {
+    const client = {};
+    chatroom.add(client);
+    const newUser = chatroom.rename(client.username, 'banana');
+    expect(newUser.username).toEqual('banana');
+  });
+
 });
 
