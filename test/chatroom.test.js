488fd1691a3c8cac5e11ff67fc3266d40fd7f089
Paige Gorry <paigeegorry@gmail.com>
2019-01-14 14:57:54 -0800

    i think my test is write

--- test/chatroom.test.js
+++ test/chatroom.test.js
@@ -11,4 +11,12 @@ describe('Chatroom', () => {
     chatroom.add(client);
     expect(client.username).toBeDefined();
   });
+  it('returns the client with said username', () => {
+    const client = {};
+    chatroom.add(client);
+    const username = client.username;
+    const chosen = chatroom.getClient(username);
+    expect(chosen.username).toEqual(client.username);
+  });
 });
+
