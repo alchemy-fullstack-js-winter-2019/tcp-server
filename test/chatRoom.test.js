b8b240c3d10ec98b861cc9dcb28532e40d0cbeb0
Carmen Perezchica <crperezchica@gmail.com>
2019-01-14 15:37:07 -0800

    add and username method written

--- test/chatRoom.test.js
+++ test/chatRoom.test.js
@@ -1,11 +1,15 @@
-const chatRoom = require ('../lib/chatRoom');
+const ChatRoom = require ('../lib/chatRoom');
 
 describe('Chat room', () => {
+  let chatRoom = null;
+  beforeEach(() => {
+    chatRoom = new ChatRoom();
+  });
   it('can add a client', () => { //add this method to our list of clients
     //const client
     const client = {};
     chatRoom.add(client);
-
+    expect(client.username).toBeDefined();
   });
-  expect(client.username).toEqual(username);
+ 
 });
