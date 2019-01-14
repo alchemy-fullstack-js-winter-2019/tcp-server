2cfa75b6712199016fd0db409b7c3a0f0fe97c9a
Aaron Dennis <AMDennis1987@gmail.com>
2019-01-14 15:39:37 -0800

    fixed all tests minus rename without callbacks

--- test/chatRoom.test.js
+++ test/chatRoom.test.js
@@ -1,10 +1,14 @@
 const ChatRoom = require('../lib/chatRoom');
 
-const client1 = {};
-const client2 = { username: 'Pete' }
+const mockMath = Object.create(global.Math);
+mockMath.random = () => 0.5;
+global.Math = mockMath;
 
 describe('ChatRoom', () => {
+  const client1 = {};
+  const client2 = {};
   let chatRoom = null;
+
   beforeEach(() => {
     chatRoom = new ChatRoom();
     chatRoom.add(client1);
@@ -12,39 +16,34 @@ describe('ChatRoom', () => {
   });
 
   it('adds a client in my chatroom', done => {
-    chatRoom.add({ username: 'Aaron' }, (err, addedClient) => {
-      expect(err).toBeFalsy();
-      expect(addedClient.username).isDefined({ username: 'Aaron' });
-    });
+    const client3 = {};
+    chatRoom.add(client3);
+    expect(client3.username).toEqual('bitter_breeze');
+    expect(client3).toBeDefined();
     done();
   });
 
   it('gets a client by username', done => {
-    chatRoom.getClient({ username: 'Aaron' }, (err, foundClient) => {
-      expect(err).toBeFalsy();
-      expect(foundClient.username).toEqual({ username: 'Aaron' });
-    });
+    chatRoom.getClient('bitter_breeze');
+    expect(client2.username).toEqual('bitter_breeze');
     done();
   });
 
   it('renames a user', done => {
-    chatRoom.rename({ username: 'Aaron' }, 'Aaron2'), (err, renamedClient) => {
-      expect(err).toBeFalsy();
-      expect(renamedClient.username, 'Aaron2').toEqual('Aaron2');
-    };
+    chatRoom.rename('Aaron', 'Aaron2');
+    expect(chatRoom.username).toEqual('Aaron2');
+
     done();
   });
 
   it('returns a list of all clients in map', done => {
-    chatRoom.all(), (err, allClients) => {
-      expect(err).toBeFalsy();
-      expect(allClients).toHaveLength(allClients.length);
-    };
+    chatRoom.all();
+    expect(chatRoom).toEqual({});
     done();
   });
 
   it('cannot rename to existing username', done => {
-    chatRoom.rename({ username: 'Aaron' }, 'Pete'), (err, cannotRename) => {
+    chatRoom.rename({ username: 'Aaron' }, 'bitter_breeze'), (err, cannotRename) => {
       expect(err).toBeFalsy();
       expect(cannotRename).toBeFalsy();
     };
