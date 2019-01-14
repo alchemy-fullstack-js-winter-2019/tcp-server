c5108246c7c2a6f6bb8ac7d030c0d57b0c5ec33a
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-14 14:58:47 -0800

    add and find by id method works

--- test/chatroom.test.js
+++ test/chatroom.test.js
@@ -17,19 +17,23 @@ describe(' test various chatroom methods', () => {
         expect(client.username).toBeDefined();
 
     });
-    it('can get a client by value', done => {
+    it('can get a client by value', () => {
         // const client = {};
         const c1 = chatroom.add({});
         const c2 = chatroom.add({});
-        const returnedClient = chatroom.getClient(c2.username);
+        const returnedClient = chatroom.getClient(c1.username);
         console.log(returnedClient);
 
-        expect(returnedClient.username).toEqual(c2.username);
-
-
-        done();
-
+        expect(returnedClient.username).toEqual(c1.username);
     });
+    it('can rename an username', () => {
+        const c1 = chatroom.add({});
+
+        const renamedClient = chatroom.rename(c1, 'bob');
+
+        expect(renamedClient.username).toEqual('bob');
+
+    })
 
 
 });
