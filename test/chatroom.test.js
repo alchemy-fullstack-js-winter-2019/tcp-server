209b1cd23f9eea69e77c99c5a8c6e1d5dd9f3a02
pickuppatterns <pickup.patterns@gmail.com>
2019-01-14 15:45:50 -0800

    adds getClient to chatroom with passing tests

--- test/chatroom.test.js
+++ test/chatroom.test.js
@@ -5,25 +5,26 @@ describe ('ChatRoom', () => {
     chatroom = new ChatRoom();
    });
 
-   it('adds a client ', () => {
+  it('adds a client ', () => {
     const client = {}; 
     chatroom.add(client);
-       expect(client.username).toBeDefined();
-     });
+    expect(client.username).toBeDefined();
+  });
   it('returns the client with said username', () => {
-      const client = {};  
-      chatroom.getClient(username);
-      expect(client).toEqual(client.username);
-      done();
+    const client = {};  
+    const addClient = chatroom.add(client);
+    const foundClient = chatroom.getClient(client.username);
+    expect(foundClient).toEqual(client);
+  });
+  it('renames the old username to new new username', () => {
+    const client = {};  
+    const addClient = chatroom.add(client);
+    //get client by old name 
+    //delete old key:value pair - use delete method
+    //add a new pair or new client
+    //set clients username to new username
+    //someClientUserName = newUsername
   })
 
-
-
-   });
-   
   
-  
-
-
-
-
+});
