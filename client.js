4caed393bcd5384b38247e056987c8956207fc11
Carmen V Ramos <carmenvramos@yahoo.com>
2019-01-14 14:06:53 -0800

    Initial Commit

---
+++ client.js
@@ -0,0 +1,29 @@
+// create a TCP client
+const { createConnection } = require ('net');
+const { createInterface } = require('readline');
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '>'
+});
+
+const client = createConnection({ port:7890 }, () => {
+  rl.prompt();
+  console.log('Connected To Server');
+  // listen to line events and write data to server
+  rl.on('line', (line) => {
+    rl.prompt();
+    // write data to server
+    client.write(line);
+    
+  });
+});
+
+// use rl to print data from server to console
+client.on('data', data => {
+  rl.write(data);
+});
+
+// client.write ('Hi there');
+// client.pipe(process.stdout);
---
+++ server.js
@@ -0,0 +1,21 @@
+
+// const createServer = require('net');
+
+// const server = createServer(client => {
+//   console.log('CLIENT CONNECTED');
+// });
+
+// server.listen(7890, () => console.log('LISTENING ON 7890'));
+
+const { createServer } = require('net');
+
+const server = createServer(client => {
+  console.log('CLIENT CONNECTED');
+  client.on('data', data => {
+    console.log(`CLIENT: ${data}`);
+  });
+});
+
+server.listen(7890, () => console.log('LISTENING ON 7890'));
+
+
