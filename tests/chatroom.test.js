f818e7fa9a862bad22110766d8e1b66be83e1ed2
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-14 14:22:44 -0800

    scaffolding the methods in chatroom.js

--- tests/chatroom.test.js
+++ tests/chatroom.test.js
@@ -2,27 +2,28 @@ const chatroom = require('../lib/chatroom');
 
 describe('ChatRoom', () => {
   let chatroom = null;
-
-});
-
-it('can add a client', done => {
-  chatroom.add({ username: 'abel' }, (err, createdClient) => {
-    expect(err).toBeFalsy;
-    expect(createdClient).toEqual({ username: 'abel' });
-    done();
-  });
-});
-
-it('asserts that a name was returned as the username property of the client', (client.username, username) => {
-  console.assert.equal(client.username, username);
-  expect(username).toEqual(client.username);
-});
-
-it('asserts that the name was assigned as a property to the client object', (client.username, username) => {
-  console.assert.equal(client.username, username);
   
-});
-
-it('passes in the username returned from the add call and asserts the object is the same as what you supplied to the add call', done => {
-
+  it('can add a client', done => {
+    //const client = {}
+    chatroom.add({ username: 'abel' }, (err, createdClient) => {
+      expect(err).toBeFalsy;
+      //expect client.username is defined
+      expect(createdClient).toEqual({ username: 'abel' });
+      done();
+    });
+  });
+  
+  it('asserts that a name was returned as the username property of the client', (client.username, username) => {
+    console.assert.equal(client.username, username);
+    expect(username).toEqual(client.username);
+  });
+  
+  it('asserts that the name was assigned as a property to the client object', (client.username, username) => {
+    console.assert.equal(client.username, username);
+    
+  });
+  
+  it('passes in the username returned from the add call and asserts the object is the same as what you supplied to the add call', done => {
+  
+  });
 });
