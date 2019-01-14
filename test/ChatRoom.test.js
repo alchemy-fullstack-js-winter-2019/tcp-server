13c3af09988b188ac1457d1c585b45278b39ccbe
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-14 15:36:43 -0800

    getClient passes

--- test/ChatRoom.test.js
+++ test/ChatRoom.test.js
@@ -1,14 +1,29 @@
 const { ChatRoom } = require('../lib/ChatRoom');
 
 describe('ChatRoom', () => {
-  
+  let chatroom = null;
+  beforeEach(() => {
+    chatroom = new ChatRoom();
+  });
+
   it('can add a client', () => {
-    const chatroom = new ChatRoom();
     //const client = {}
     const client = {};
     //call the chatroom.add(client)
     chatroom.add(client);
     // expect client.username is defined
-    expect(client.username).toEqual('user0');
+    expect(client.username).toEqual(1);
   });
+
+  it('can get an existing client', () => {
+    const c1 = chatroom.add({});
+    const returnedClient = chatroom.getClient(c1.username);
+    expect(returnedClient).toEqual(c1);
+    // expect(client.username).toEqual('user');
+  });
+
+  it('should fail if username doesn\'t exist', () => {
+
+  });
+
 });
