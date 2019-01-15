ea92bcfed32a3fa80054f47244926b6286a68820
Paige Gorry <paigeegorry@gmail.com>
2019-01-14 17:38:15 -0800

    switching over to app.js and server.js

--- test/parseMessage.test.js
+++ test/parseMessage.test.js
@@ -1,9 +1,17 @@
 const parseMessage = require('../lib/parseMessage');
 
 // named groups
+describe('parseMessage', () => {
+  it.only('returns an object with command, arg, and text', () => {
+    expect(parseMessage('@cmd: param some text')).toEqual({
+      command: 'cmd',
+      arg: 'param',
+      text: 'some text'
+    });
+  });
 
-expect(parseMessage('@cmd:param some text')).toEqual({
-  command: 'cmd',
-  arg: 'param',
-  text: 'some text'
+  it('ignores strings that do not start with @', () => {
+    expect(parseMessage('banana')).toThrowError();
+  });
+
 });
