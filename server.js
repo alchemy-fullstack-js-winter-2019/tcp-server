c8fb90d88ba69274d03144ec0d6a27be3b56dcaf
Marcy Silverman <marcysilve225@gmail.com>
2019-01-14 20:44:37 -0800

    changed server

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = require('./lib/app');
 const PORT = 7890;
 
 /* eslint-disable-next-line */
-app.listen(() => console.log('Server listening on 7890'), PORT);
+app.on('listening', () => console.log('Server listening on 7890'), PORT);
 
-// app.listen(PORT);
+app.listen(PORT);
