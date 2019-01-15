39a498f802eed05717d10e5b367d07a3e2d8f833
pickuppatterns <pickup.patterns@gmail.com>
2019-01-14 17:07:37 -0800

    adds parseMessage & parseMessageTest, also adds rename method

--- test/chatroom.test.js
+++ test/chatroom.test.js
@@ -1,30 +1,46 @@
-const ChatRoom = require('../lib/chatroom')
+const ChatRoom = require('../lib/chatroom');
 
 describe ('ChatRoom', () => { 
-   beforeEach(() => {
+  beforeEach(() => {
     chatroom = new ChatRoom();
-   });
+  });
 
   it('adds a client ', () => {
     const client = {}; 
     chatroom.add(client);
+    
     expect(client.username).toBeDefined();
   });
+
   it('returns the client with said username', () => {
     const client = {};  
     const addClient = chatroom.add(client);
     const foundClient = chatroom.getClient(client.username);
+   
     expect(foundClient).toEqual(client);
   });
+  
   it('renames the old username to new new username', () => {
     const client = {};  
     const addClient = chatroom.add(client);
-    //get client by old name 
+    //get oldName from client  
+    const renameUser = chatroom.rename(client.username, 'bob');
     //delete old key:value pair - use delete method
-    //add a new pair or new client
-    //set clients username to new username
-    //someClientUserName = newUsername
-  })
 
+    
+    expect(renameUser).toEqual('bob');
+
+    //add a new pair or new client
+    //set clients username to newUsername
+    //someClientUserName = newUsername
+    //expect that the call to .rename returns true
+    //expect that using the old username does not return the client
+    //expect that using the new username does return the client
+    //expect that the client.username is now equal to the new username 
+  });
+  
+  it('returns all clients', () => {
+
+  });
   
 });
