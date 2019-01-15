4e29a1820fa9a5982abfe3e8f3cb797219a4336e
Masmics <cherlocker3113@gmail.com>
2019-01-14 18:12:52 -0800

    end of class commit

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+const ChatRoom = require('.lib/chatRoom');
+const { createServer } = require('net');
 //create primary tcp server here. it needs to:
 // **Create a ChatRoom instance to manage chat users
 // **Call net.createServer to create the server and listen for events
 // **Subscribe to client socket events
 // **(Remember to call client.setEncoding('utf8'); to work with just text)
-// **Properly orchestrate connected clients.
 
-// const chatRoom = new ChatRoom();
+const chatRoom = new ChatRoom();
 
-// createServer(7890, client => {
-//   client.on(data, () => {
+createServer(7890, client => {
+  //add the client to char room (see example from class)
+  client.on(data, () => {
+    //parse message (get the command)
+    //if or else if or else or switch block, on the parseMessage command
+    //if DM, look up client based on username found in arg: key - lookup
+    //send that client a message
+    //if @all, use chatRoom.all to get list of all clients & send each client the msg(found in text key) filter out the client who sent it (eg .filter)
 
-//   });
-// });
+    //if @nick, chatRoom.rename with the newName (found in arg: key)
+    client.setEncoding('utf8');
+  });
+  //when a client disconnects, remove them from chat room
+});
+module.exports = server;
--- test/parseMessagenottest.js
+++ test/parseMessagenottest.js
@@ -1,9 +1,17 @@
-const ParseMessage = require('../lib/parseMessage');
+const parseMessage = require('../lib/parseMessage');
 
-describe('parses messages', () => {
-  const message = new ParseMessage;
-  it('ignores strings that do not start with "@"', () => {
-    expect(message.charAt[0]).includes('@');
+describe('parseMessage', () => {
+  //const incomingTestString1 = '@all '
+  // const message = new ParseMessage;
+  // it('ignores strings that do not start with "@"', () => {
+  //   expect(message.charAt[0]).includes('@');
+  // });
+  it('takes strings starting with "@" and returns a parsed object', () => {
+    expect(parseMessage('@cmd:param some text')).toEqual({
+      command: 'cmd',
+      arg: 'param',
+      text: 'some text'
+    });
   });
 });
 
