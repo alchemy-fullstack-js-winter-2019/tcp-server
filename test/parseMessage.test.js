9672679b731ff3266dc99b9a7486bacb294fa138
Kate Dameron <katerj33@gmail.com>
2019-01-14 17:21:11 -0800

    message parsing function correctly parses string

--- test/parseMessage.test.js
+++ test/parseMessage.test.js
@@ -1,11 +1,14 @@
-const message = require('./lib/parseMessage');
+const message = require('../lib/parseMessage');
 
-describe('parseMsg', () => {
+describe('message', () => {
   it('it takes a string and returns an object based on msg contents', () => {
-    expect(parseMsg('@cmd:param some text')).toEqual({
-      command: 'cmd',
-      arg: 'param',
-      text: 'some text'
-    });
+    expect(message('@cmd:param some text')).toEqual(
+      {
+        command: 'cmd',
+        arg: 'param',
+        text: 'some text'
+      });
   });
+
+  
 });
