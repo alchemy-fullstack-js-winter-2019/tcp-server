608e33544d585c6b2d115d2c7053a5c24ab44859
pickuppatterns <pickup.patterns@gmail.com>
2019-01-14 17:34:26 -0800

    adds logic for parseMessage

---
+++ test/parseMessage.test.js
@@ -0,0 +1,9 @@
+describe('parseMessage', () => {
+  it('parse the components out of a string', ()
+  expect(parseMessage('@cmd:param some text'))
+    command: 'cmd',
+    arg: 'para',
+    text:'some text'
+    });
+  });
+});
