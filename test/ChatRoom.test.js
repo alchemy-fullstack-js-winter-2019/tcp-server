3e9ebc59bb643ee1433e2ee54fd3ca24b4f1da78
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-14 16:33:15 -0800

    rename method works

--- test/ChatRoom.test.js
+++ test/ChatRoom.test.js
@@ -17,20 +17,22 @@ describe('chatroom tests', () => {
   it('returns the user that is associated with a username', () => {
     const c1 = chatroom.add({});
     const returnedClient = chatroom.getClient(c1.username);
-    console.log(returnedClient);
 
     expect(returnedClient).toEqual(c1);
   });
 
   it('renames a user', () => {
+    const c1 = chatroom.add({});
+
     
-    
+    chatroom.rename(c1.username, 69);
+
+    const results = chatroom.getClient(69);
 
 
-    expect(chatroom.rename(1, 69)).toEqual(true);
-    expect(chatroom.getClient(1)).toEqual(undefined);
+    expect(chatroom.rename(1, 69)).toBeTruthy();
     expect(chatroom.getClient(69)).toEqual({ username: 69 });
-    expect(client.username).toEqual(69);
+    expect(results.username).toEqual(69);
   });
 });
 
