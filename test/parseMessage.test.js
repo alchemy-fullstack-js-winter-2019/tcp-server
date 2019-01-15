5e3e2a83a5a640467459f8762d5ebbb4d3021633
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-14 21:16:25 -0800

    playing with parseMessage function, added tests too

--- test/parseMessage.test.js
+++ test/parseMessage.test.js
@@ -13,4 +13,20 @@ describe('parseMessage', () => {
     });
   });
 
+  it('parses the components out of an @all message string', () => {
+    expect(parseMessage('@all hello world!')).toEqual({
+      command: 'all',
+      arg: '',
+      text: 'hello world!'
+    });
+  });
+
+  it('parses the components out of an @dm message string', () => {
+    expect(parseMessage('@dm:exampleuser Hello this is a direct message')).toEqual({
+      command: 'dm',
+      arg: 'exampleuser',
+      text: 'Hello this is a direct message'
+    });
+  });
+
 });
