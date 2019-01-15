8c9cd7cfd8d1c90957f10f081c05f307defbf5dd
Ivan <i.poblete915@gmail.com>
2019-01-14 16:55:29 -0800

    rename test passed

--- test/chatRoom.test.js
+++ test/chatRoom.test.js
@@ -5,11 +5,19 @@ describe('Chatroom', () => {
   beforeEach(() => {
     chatRoom = new ChatRoom();
   });
+
   it('can add a client', () => {
     const client = {};
-    // call the chatroom.add(client)
     chatRoom.add(client);
-    // expect client.username is defined
     expect(client.username).toBeDefined;
+  });
+
+  it('renames a user', () => {
+    const client = {};
+    chatRoom.add(client);
+    const originalName = client.username;
+    expect(chatRoom.rename(client.username, 'new')).toBeTruthy;
+    expect(client.username).toEqual('new');
+    expect(chatRoom.getClient(originalName)).toBeFalsy;
   });
 });
