12cedf0ee7440075ab0d193e104537697bfdf060
Marcy Silverman <marcysilve225@gmail.com>
2019-01-14 21:21:00 -0800

    parse message is working

--- test/parseMessage.test.js
+++ test/parseMessage.test.js
@@ -1,13 +1,26 @@
 const parseMessage = require('../lib/parseMessage');
 
 describe('parseMessage', () => {
-  it('should exclude @', () => {
-    expect(parseMessage('whats up')).toEqual(null);
+  it('ignores chat commands that do not start with: @', () => {
+    let response = parseMessage('Hello all`?');
+    expect(response).toEqual(null);
   });
   it('atAll works', () => {
-    expect(parseMessage('@all hi there')).toEqual({
-      command: '@',
-      text: 'all hi there'
-    });
+    let response = parseMessage('@all: whats up everybody');
+    let result = {
+      command: 'all',
+      arg: '',
+      text: 'whats up everybody'
+    };
+    expect(response).toEqual(result);
+  });
+  it('nick name', () => {
+    let response = parseMessage('@nick: Kitten whats up everybody');
+    let result = {
+      command: 'nick',
+      arg: '',
+      text: 'Kitten whats up everybody'
+    };
+    expect(response).toEqual(result);
   });
 });
