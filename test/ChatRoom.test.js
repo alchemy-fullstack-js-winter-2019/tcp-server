1bc6087ce085d2de3177bbff2477b4100385868f
Carmen V Ramos <carmenvramos@yahoo.com>
2019-01-16 06:33:47 -0800

    Added more message tests to handle chatroom commands

--- test/ChatRoom.test.js
+++ test/ChatRoom.test.js
@@ -2,38 +2,44 @@ const ChatRoom = require('../lib/ChatRoom');
 
 describe('ChatRoom', () => {
 
+  let chatroom = null;
+
+  beforeEach(() => {
+    chatroom = new ChatRoom();
+  });
+
   it('can add a client', () => {
     const client = {};
-    const chatroom = new ChatRoom();
     chatroom.add(client);
     expect(client.username).toBeDefined();
   });
   
   it('gets the client with said username', () => {
     const client = {};
-    const chatroom = new ChatRoom();
     chatroom.add(client);
-
     const gotClient = chatroom.getClient(client.username);
     expect(gotClient).toEqual(client);
   });
 
   it('renames username to newusername', () => {
     const client = {};
-    const chatroom = new ChatRoom();
     chatroom.add(client);
     const origName = client.username;
     expect(chatroom.rename(client.username, 'Carmen')).toBeTruthy();
     expect(client.username).toEqual('Carmen');
     expect(chatroom.getClient(origName)).toBeFalsy();
-    
   });
+
+  // it('cannot rename to existing username', () => {
+  //   const client = {};
+  //   chatroom.add(client);
+  //   expect(chatroom.rename(client.username, 'user0')).toBeFalsy();
+  // });
 
   it('gets all clients', () => {
     const chatroom = new ChatRoom();
     const c1 = chatroom.add({});
     const c2 = chatroom.add({});
-
     const allClients = chatroom.all();
     expect(allClients).toEqual([c1, c2]);
 
--- test/parseMessage.test.js
+++ test/parseMessage.test.js
@@ -9,6 +9,33 @@ describe('parseMessage', () => {
     });
   });  
 
+  it('handles @nick command', () => {
+    const parsedMsg = parseMessage('@nick:Menchu');
+    expect(parsedMsg).toEqual({
+      command: 'nick',
+      arg: 'Menchu',
+      text: ''
+    });
+  });
+
+  it('handles @dm command', () => {
+    const parsedMsg = parseMessage('@dm:Menchu hi there!');
+    expect(parsedMsg).toEqual({
+      command: 'dm',
+      arg: 'Menchu',
+      text: 'hi there!'
+    });
+  });
+
+  it('handles @all command', () => {
+    const parsedMsg = parseMessage('@all Hello all!');
+    expect(parsedMsg).toEqual({
+      command: 'all',
+      arg: undefined,
+      text: 'Hello all!'
+    });
+  });
+
   it('Ignores strings that do not start with @', () => {
     const msg = 'How is everyone doing?';
     expect(parseMessage(msg)).toEqual(null);
