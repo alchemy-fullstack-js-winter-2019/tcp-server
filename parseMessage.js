5fb8c62efc24bba70758268e0a4e5ef09f0b41a6
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-17 12:45:28 -0800

    refactored parseMessage.js //TODO: Replace regex

--- parseMessage.js
+++ parseMessage.js
@@ -1,15 +1,29 @@
-module.exports = message => {
-  function message(str) {
-    if(str.startsWith('@') = false) return null;
-  }
+// module.exports = message => {
+//   function message(str) {
+//     if(str.startsWith('@') === false) return null;
+//   }
 
-  const actualMessage = message.slice(1);
-  actualMessage.indexOf(':');
+//   const actualMessage = message.slice(1);
+//   actualMessage.indexOf(':');
+
+//   return {
+//     command: 'all',
+//     args: 'nick',
+//     text: 'hello'
+//   };
+// };
+
+module.exports = str => {
+  if(str[0] !== '@') return null;
+
+  const pattern = /@(?<command>\w*):?(?<arg>\w*)\s?(?<text>.*)?/;
+  const match = pattern.exec(str);
+
+  if(!match || !match.groups) return null;
 
   return {
-    command: 'all',
-    args: 'nick',
-    text: 'hello'
+    command: match.groups.command,
+    arg: match.groups.arg,
+    text: match.groups.text
   };
 };
-
