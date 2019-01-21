a80c395b4bb0b2846135ed56fb3bedad1d6cbf64
Carmen Perezchica <crperezchica@gmail.com>
2019-01-20 21:50:35 -0800

    cleaned server.js and client.js files

--- client.js
+++ client.js
@@ -7,34 +7,19 @@ const rl = createInterface({
   prompt: '>'
 });
 
-
-const client = createConnection(7890, () => { //{port: 7890}
+const client = createConnection(7890, () => { 
   rl.prompt();
   console.log('connected to server!');
-  // stream.pipe(process.stdout);
-  //this callback triggered after the connection is established
-  //listen to line events and write data to server
   rl.on('line', line => { //listen to readline interface
-    //write data to server
+    client.write(line);
     rl.prompt();
-    client.write('hi');
-    // client.on('data', (data) => {
-
-    // })
-    // console.log(`Received: ${input}`);
-    // rl.close();
   });
 });
 
-//listen to messags from the server
-//use readline to print data from server to console
-client.on('data', data => { //listen to data coming from the server - client sending to server
-  rl.write(data); //writing to whatever output we specify in the interface
+client.setEncoding('utf8');
+
+client.on('data', data => { 
+  rl.write(data); 
 });
 
-// client.write('hi there'); //client sending message to server
 
-// client.pipe(process.stdout);
-
-
-  //read and write duplex strings on both the client and server side 
