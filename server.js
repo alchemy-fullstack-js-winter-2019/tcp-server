fbc32f7bde3b3de83aeb1fe4bc3b1a0d272a6b37
JeiWolfei <jvampy22@gmail.com>
2019-01-22 23:07:09 -0800

    deleted classwork from files

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const server = require('./lib/app');
 const PORT = 3333;
 
 server.listen(PORT, () => {
+  // eslint-disable-next-line
   console.log(`Server started on ${PORT}`);
 });
--- tcp.js
+++
@@ -1,8 +0,0 @@
-const { createServer } = require('net');
-
-const server = constServer(client => {
-  console.log('CLIENT CONNECTED');
-});
-
-server.listen(7890, () => console.log('LISTENING ON 7890'));
-
