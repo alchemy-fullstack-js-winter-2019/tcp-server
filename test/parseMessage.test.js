b5c7e69e54b580e35f20d337389c530724d7f83c
JeiWolfei <jvampy22@gmail.com>
2019-01-22 23:04:17 -0800

    fixed pareseMessage test

--- test/parseMessage.test.js
+++ test/parseMessage.test.js
@@ -1,36 +1,16 @@
 const parseMessage = require('../lib/parseMessage');
 
 describe('parseMessage', () => {
-  it('can ignore strings that do not start with "@"', () => {
-    const response = parseMessage('cmd:param some text');
-    expect(response).toEqual(null);
-  });
-
-  it('can return a command object based on message contents', () => {
-    const message = '@cmd:param some text';
-    const response = parseMessage(message);
-    const expected = {
+  it('returns an object with command, arg, and text', () => {
+    expect(parseMessage('@cmd: param some text')).toEqual({
       command: 'cmd',
       arg: 'param',
       text: 'some text'
-    };
-    expect(response).toEqual(expected);
+    });
   });
 
-  it('is functional with the dm command', () => {
-    const message = '@dm:ok kids, here we are.';
-    const response = parseMessage(message);
-    expect(response).toEqual({ command: 'dm', arg: 'is it working', text: 'some text' });
+  it('ignores strings that do not start with @', () => {
+    expect(parseMessage('banana')).toEqual(null);
   });
 
-  it('is functional with the nick command', () => {
-    const message = '@nick:jj';
-    const response = parseMessage(message);
-    expect(response).toEqual({ command: 'nick', arg: 'jj', text: '' });
-  });
-
-  it('is functional with the all command', () => {
-    const message = '@all howdy';
-    const response = parseMessage(message);
-    expect(response).toEqual({ command: 'all', arg: '', text: 'hi' });
-  });
+});
